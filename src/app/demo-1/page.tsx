a333b38e633b1722725b9603629dc156b723c395
morjuax <morjuax@C02G42JEMD6N.local>
2023-09-23 18:55:37 -0300

    style: Navigation components

--- src/app/demo-1/page.tsx
+++ src/app/demo-1/page.tsx
@@ -1,5 +1,30 @@
 export default function demo1() {
   return (
-    <h1>demo 1</h1>
+    <>
+      <h1 className="mb-3">Demo 1</h1>
+      <div className="relative flex flex-wrap place-items-center ">
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 1
+        </div>
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 2
+        </div>
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 3
+        </div>
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 4
+        </div>
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 5
+        </div>
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 6
+        </div>
+        <div className="relative p-20 border-2 border-dotted mr-1 mb-2">
+          here drag & drop 7
+        </div>
+      </div>
+    </>
   )
 }
